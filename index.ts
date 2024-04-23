e307c9092eed7d80108300f25553e011a165c4ef
ambroisegithub <muhayimana21@gmail.com>
2024-04-23 12:26:40 +0200

    project initialization

---
+++ index.ts
@@ -0,0 +1,20 @@
+import bodyParser from "body-parser";
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import morgan from "morgan";
+const app: Application = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(morgan("dev"));
+
+app.get("/", (req: Request, res: Response) => {
+  return res.json({ message: "Welcome To The Dynamites backend e-commerce" });
+});
+
+const PORT: number = 3000;
+app.listen(PORT, () => {
+  console.log(`The App is running on ${PORT}`);
+});
+
