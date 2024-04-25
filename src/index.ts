3e50662eb707033f26c0dc0df8f25d915bdfeb7f
Eddy <eddy@Eddys-MacBook-Pro.local>
2024-04-25 14:24:06 +0200

    Linters and formating are now working

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,20 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-import morgan from "morgan";
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
 import swaggerUi from 'swagger-ui-express';
-import swaggerSpec from "./docs/swaggerconfig";
+import swaggerSpec from './docs/swaggerconfig';
 
 const app: Application = express();
 
 app.use(cors());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-
-app.get('/', (req: Request, res: Response) => res.json({ message: 'Welcome To The Dynamites backend e-commerce' }));
+app.get('/', (req: Request, res: Response) =>
+  res.json({ message: 'Welcome To The Dynamites backend e-commerce' })
+);
 
 const PORT: number = 3000;
 app.listen(PORT, () => {
-  console.log(`The App is running on ${PORT}`)
-
+  console.log(`The App is running on ${PORT}`);
 });
-
