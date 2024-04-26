9f2d29e622eea7fbda11da3bc4e996f8e4cb11de
13XAVI <tresorxavier16@gmail.com>
2024-04-26 13:19:47 +0200

    removed dist on ignore ch dotenv #14

--- dist/types/controller/user.d.ts
+++
@@ -1,3 +0,0 @@
-import { User } from '../database/models';
-export declare const createUser: (data: any) => Promise<User>;
-export declare const allUsers: () => Promise<User[]>;
--- dist/types/database/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { DataSource } from 'typeorm';
-declare const dbConnection: DataSource;
-export default dbConnection;
-export declare const connect: () => Promise<void>;
