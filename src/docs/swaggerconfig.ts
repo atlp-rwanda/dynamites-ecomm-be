b92eb3865c9854ff1ad69babaae23a0caf0220c6
13XAVI <tresorxavier16@gmail.com>
2024-04-26 09:16:37 +0200

    ch configure dotenv #14

--- src/docs/swaggerconfig.ts
+++ src/docs/swaggerconfig.ts
@@ -10,7 +10,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: process.env.APP_URL,
         description: 'Development server',
       },
     ],
