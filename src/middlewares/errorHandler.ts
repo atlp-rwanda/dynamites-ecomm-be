d00fd913000e8d01e5a73fb127fcd4d3a20a688e
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2024-05-02 15:51:43 +0000

    Removed unnecesary type annotation and console logs in errorHandler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -5,8 +5,7 @@ function errorHandler(func: Function) {
     return async (req: Request, res: Response, next: NextFunction) => {
       try {
         await func(req, res, next);
-      } catch (error: any) {
-        console.log({ error });
+      } catch (error) { // Removed the type annotation from the catch clause variable because it caused liting errors
         const message = error.detail || 'Internal Server Error';
         res.status(500).send(message);
       }
