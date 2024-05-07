9082fc4af3390829f7fc046ad185faaee33a0d85
Joslyn Manzi Karenzi <j.karenzi@alustudent.com>
2024-05-07 20:25:43 +0200

    * feat(rbac): Implement role based access control
    -define roles and permissions for vendors and buyers
    -assign roles and permissions to users during registration or profile update
    -enforce role-based access control throughout the application
    -write comprehensive unit tests
    
    [Delivers #34]
    
    * feat(rbac): integrate rbac into user registration
    
    -integrate role based access control into user registration
    
    [Delivers #34]
    
    * feat(rbac): integrate rbac into user registration
    
    -integrate role based access control into user registration
    
    [Delivers #34]
    
    ---------
    
    Co-authored-by: ambroisegithub <muhayimana21@gmail.com>
    
    Social Logins (#45)
    
    * squashing commits
    
    implementing routes for auth
    
    create passport callback function
    
    adding new user from Google
    
    creating new user
    
    check if user is exist in db
    
    implementing cookie session
    
    Fix error of TypeError: req.session.regenerate is not a function using Passport
    
    fix secret keys
    
    remove Google client secret keys
    
    working on facebook strategy
    
    get email from fb login and update the scope
    
    after verification save the user into db
    
    add profile image in db
    
    fixing minor bugs
    
    fix minor bugs in codes
    
    after rebasing & updating some fts
    
    link social login with userModel
    
    Addong Google client keys & FB client key into yml
    
    send confrim email after register a new user
    
    send email after register from facebook
    
    fix minor bugs
    
    * fix minor errors
    
    * remove lints errors
    
    user register
    
    register user test
    
    register user testing fix
    
    register user testing fix
    
    register user testing fix
    
    Authentication for User
    
    Added slint changes
    
    removed  mocha
    
     added new features
    
     added new features
    
    Solved comflicts
    
    changed file
    
    added changes
    
    added new Test
    
    added new Test
    
    resolved test cases
    
    resolved test cases
    
    implemented two-factor authentication for enhanced security
    
    implemented two-factor authentication for enhanced security
    
    check whether the usertype is vendor to proceed with 2FA
    
    test the 2fa authentication
    
    Design the database schema for storing seller products and related information
    
    create endpoints for category and also database schema
    
    fix routes for products & categories
    
    implement CRUD operations for category entity
    
    create swagger docs for category routes
    
    adds delete  documentation for category routes
    
    complete documentation for category routes
    
    implementing craate new product route and its controller
    
    implementing get product route and its controller
    
    working on update function
    
    Extend API endpoints
    
    store reference ID of vendor
    
    ft-Product-Docs
    
    This PR add the product swagger docs and some test category
    
    bug-documentation-fixes
    
    Fixing the errors on the doumentation
    
    add new tests for buyers login
    
    Design the database schema for storing seller products and related information
    
    create endpoints for category and also database schema
    
    fix routes for products & categories
    
    implement CRUD operations for category entity
    
    create swagger docs for category routes
    
    adds delete  documentation for category routes
    
    complete documentation for category routes
    
    implementing craate new product route and its controller
    
    implementing get product route and its controller
    
    working on update function
    
    Extend API endpoints
    
    store reference ID of vendor
    
    ft-Product-Docs
    
    This PR add the product swagger docs and some test category
    
    bug-documentation-fixes
    
    Fixing the errors on the doumentation
    
    store reference ID of vendor
    
    replacing try & catch with errorHandler middleware
    
    remove all try & catch blocks in product controller
    
    validating routes
    
    fix minor errors in test categories
    
    Category-testing
    
    add category testing cases
    
    init commit for prouct test
    
    product controller test
    
    complete product test
    
    merging all commits
    
    delete product data after testing
    
    add --detectOpenHandles flag

---
+++ src/routes/categoryRoutes.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import {
+  createCategory,
+  deleteCategory,
+  getAllCategories,
+  getCategory,
+  updateCategory,
+} from '../controller/categoryController';
+import { IsLoggedIn } from '../middlewares/isLoggedIn';
+
+const categoryRouter = Router();
+
+categoryRouter
+  .route('/')
+  .post(IsLoggedIn, createCategory)
+  .get(getAllCategories);
+categoryRouter
+  .route('/:categoryId')
+  .get(getCategory)
+  .put(IsLoggedIn, updateCategory)
+  .delete(IsLoggedIn, deleteCategory);
+
+export default categoryRouter;
