61172ac0b380390f42cde2039be21b3685d0c1f5
wayneleon1 <rurangwaleon10@gmail.com>
2024-05-12 10:54:25 +0200

    adds delete  documentation for category routes

--- src/docs/categoryDocs.ts
+++ src/docs/categoryDocs.ts
@@ -70,3 +70,64 @@
  *                   type: string
  *                   description: A message indicating the name of category already exists
  */
+/**
+ * @swagger
+ * /api/v1/category/:
+ *   get:
+ *     summary: Get all categories
+ *     tags: [Category]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       '200':
+ *         description: Successful
+ *       '404':
+ *         description: Categories not found
+ *       '500':
+ *         description: Internal Server Error
+ */
+/**
+ * @swagger
+ * /api/v1/category/{categoryId}:
+ *   get:
+ *     summary: Get an existing category
+ *     tags: [Category]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: categoryId
+ *         type: string
+ *         required: true
+ *         description: ID of the category
+ *     responses:
+ *       '200':
+ *         description: Successful
+ *       '404':
+ *         description: category not found
+ *       '500':
+ *         description: Internal Server Error
+ */
+
+/**
+ * @swagger
+ * /api/v1/category/{categoryId}:
+ *   delete:
+ *     summary: Deletes an existing category
+ *     tags: [Category]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: categoryId
+ *         type: string
+ *         required: true
+ *         description: ID of the category to delete
+ *     responses:
+ *       '200':
+ *         description: Delete Successful
+ *       '404':
+ *         description: category not found
+ *       '500':
+ *         description: Internal Server Error
+ */
