582a8a20b55af74b489be167ba6e1d6c9cda3acd
niyibi250 <ericniyibizi1998@gmail.com>
2024-05-14 10:17:08 +0200

    ft(backend): testing of get available products -am testing status code -type of content-type if json -status to be success #57

--- src/__test__/getavailableproducts.test.ts
+++ src/__test__/getavailableproducts.test.ts
@@ -1,14 +1,43 @@
 import request from 'supertest';
-import app from '../app'
+import app from '../app';
+import { afterAllHook, beforeAllHook } from './testSetup';
+import jwt from 'jsonwebtoken';
+import dbConnection from '../database';
+import  UserModel  from '../database/models/userModel';
+const userRepository = dbConnection.getRepository(UserModel);
+
+beforeAll(beforeAllHook);
+afterAll(afterAllHook);
 
 describe('GET /api/v1/product/getAvailableProducts', () => {
   it('should return status 200 with available products', async () => {
+
     const response = await request(app).get('/api/v1/product/getAvailableProducts');
     expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty('status', 'success');
-    expect(response.body).toHaveProperty('availableProducts');
-    expect(response.body).toHaveProperty('totalPages');
-    expect(response.body).toHaveProperty('currentPage');
   });
 
-});
+  it('should return json', async()=>{
+    const response = await request(app).get('/api/v1/product/getAvailableProducts');
+    expect(response.headers['content-type']).toContain('application/json')
+  })
+
+  it('should return status success', async()=>{
+    const response = await request(app).get('/api/v1/product/getAvailableProducts');
+    expect(response.body).toHaveProperty('status', 'success');
+  })
+
+  it('should return availableProducts', async()=>{
+    const response = await request(app).get('/api/v1/product/getAvailableProducts');
+    expect(response.body).toHaveProperty('availableProducts');
+  })
+
+  it('should return totalPages', async()=>{
+    const response = await request(app).get('/api/v1/product/getAvailableProducts');
+    expect(response.body).toHaveProperty('totalPages');
+  })
+
+  it('should return currentPage', async()=>{
+    const response = await request(app).get('/api/v1/product/getAvailableProducts');
+    expect(response.body).toHaveProperty('currentPage');
+  })
+})
