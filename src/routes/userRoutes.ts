af126a4a192df6dddda5431b4bf5fa1d446e8746
HABINEZA Janvier <44426710+Habinezajanvier@users.noreply.github.com>
2024-05-17 18:30:29 +0200

    Revert "update user profile"

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -7,19 +7,15 @@ import {
   deleteUser,
   Login,
   verify2FA,
-  updateProfile
 } from '../controller/userController';
 
-const route = Router();
-route.post('/register', registerUser);
-route.get('/getAllUsers', getAllUsers);
-route.get('/confirm', confirmEmail);
-route.delete('/delete/:id', deleteUser);
-route.delete('/deleteAllUsers', deleteAllUsers);
-route.post('/login',Login)
-route.get('/all-users', getAllUsers);
-route.post('/verify2FA/:userId', verify2FA);
+const userRouter = Router();
+userRouter.post('/register', registerUser);
+userRouter.get('/getAllUsers', getAllUsers);
+userRouter.get('/confirm', confirmEmail);
+userRouter.delete('/delete/:id', deleteUser);
+userRouter.delete('/deleteAllUsers', deleteAllUsers);
+userRouter.post('/login', Login);
+userRouter.post('/verify2FA/:userId', verify2FA);
 
-route.put('/updateProfile/:id',updateProfile);
-export default route;
-
+export default userRouter;
