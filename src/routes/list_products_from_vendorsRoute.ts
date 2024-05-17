eafad189466eb4460b85337714c6f96b4f63820c
niyibi250 <ericniyibizi1998@gmail.com>
2024-05-17 10:07:28 +0200

    fix(backend): fix linting error. -remove all unused
    Feat: Buyer should be able to list products from vendors #57

--- src/routes/list_products_from_vendorsRoute.ts
+++ src/routes/list_products_from_vendorsRoute.ts
@@ -1,6 +1,4 @@
 import { Router } from 'express';
-import { IsLoggedIn } from '../middlewares/isLoggedIn';
-import { checkRole } from '../middlewares/authorize';
 import AvailableProducts from '../controller/list_products_from_vendorsController';
 const available_products_route=Router()
 
