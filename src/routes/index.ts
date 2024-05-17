1822460b0fba45b7e97e0340e9835d64b3689765
niyibi250 <ericniyibizi1998@gmail.com>
2024-05-17 09:39:13 +0200

    ft(backend):establish route,test and swagger doc -establish get all availble products -with pagination -write test of route -write swagger doc
    Feat: Buyer should be able to list products from vendors #57

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,12 +3,14 @@ import userRouter from './userRoutes';
 import roleRoutes from './roleRoutes';
 import productRoutes from './productRoutes';
 import categoryRoutes from './categoryRoutes';
+import available_products_route from './list_products_from_vendorsRoute'
 
 const router = Router();
 
 router.use('/user', userRouter);
 router.use('/roles', roleRoutes);
 router.use('/product', productRoutes);
+router.use('/getAvailableProducts',available_products_route)
 router.use('/category', categoryRoutes);
 
 export default router;
