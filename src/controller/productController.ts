8a98269a9c99ce7c0559b772b7b21321d9e776f2
niyibi250 <ericniyibizi1998@gmail.com>
2024-05-20 17:21:18 +0200

    ft (back): buyer be able to view availble product - establish route - establish controller -establish test
    Feat: Buyer should be able to list products from vendors #57

--- src/controller/productController.ts
+++ src/controller/productController.ts
@@ -324,3 +324,33 @@ export const getRecommendedProducts = errorHandler(
     });
   }
 );
+
+export const AvailableProducts =   errorHandler(async (req: Request, res: Response) => {
+  let limit: number
+  let page: number
+
+  if(req.query.limit == undefined && req.query.page == undefined) 
+    {
+    limit=10
+    page=1
+    }
+  else{
+    limit=parseInt(req.query.limit as string)
+    page=parseInt(req.query.page as string)
+  }
+  const [availableProducts, totalCount] = await productRepository.findAndCount({
+      where:{isAvailable:true},
+      take: limit,
+      skip: (page - 1) * limit,
+      select:{vendor:{firstName:true,lastName:true,picture:true}},
+      relations: ['category','vendor'],
+  });
+
+  return res.status(200).json({
+      status: 'success',
+      message: 'Items retrieved successfully.',
+      availableProducts,
+      totalPages: Math.ceil(totalCount / limit),
+      currentPage: page
+  });
+}) 
