c9183986812d5cc5602b0bb0c32279bcb8135192
wayneleon1 <rurangwaleon10@gmail.com>
2024-05-24 21:40:27 +0200

    fix: resolve deployment issue by updating configuration settings

--- src/config/db.ts
+++ src/config/db.ts
@@ -28,12 +28,14 @@ const staging = {
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
+  name: process.env.DB_NAME,
 };
 const production = {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
+  name: process.env.DB_NAME,
 };
 
 const config: {
