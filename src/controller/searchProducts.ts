dd394da7ded10870540faa28062e91e2d00cd140
Joslyn Manzi Karenzi <j.karenzi@alustudent.com>
2024-07-17 08:15:21 +0200

    fix(search): fix bugs in search functionality (#149)
    - fix bugs in search functionality
    - add new search criteria
    - add pagination
    
    [Fixes #148]

--- src/controller/searchProducts.ts
+++ src/controller/searchProducts.ts
@@ -1,15 +1,30 @@
 import { Request, Response } from 'express';
 import dbConnection from '../database';
 import Product from '../database/models/productEntity';
-import Category from '../database/models/categoryEntity';
 import errorHandler from '../middlewares/errorHandler';
+import { SelectQueryBuilder } from 'typeorm';
 
 const productRepository = dbConnection.getRepository(Product);
-const categoryRepository = dbConnection.getRepository(Category);
+
+interface searchParams {
+  keyword?: string;
+  category?: number[];
+  productName?: string;
+  rating?: number[];
+  minPrice?:number;
+  maxPrice?:number;
+  sort?: string;
+  page?:number;
+  limit?:number;
+}
+
+export const paginate = (query: SelectQueryBuilder<Product>, page: number, limit: number) => {
+  return query.skip((page - 1) * limit).take(limit);
+};
 
 export const searchProducts = errorHandler(
   async (req: Request, res: Response) => {
-    const { keyword, category, productName, sort } = req.query;
+    const { keyword, category, productName, rating, minPrice, maxPrice, sort='DESC', page=1, limit=9 }: searchParams = req.query;
 
     let queryBuilder = productRepository.createQueryBuilder('product');
 
@@ -20,35 +35,38 @@ export const searchProducts = errorHandler(
       );
     }
 
-    if (category) {
-      const categoryEntity = await categoryRepository.findOne({
-        where: { name: category as string },
-      });
-      if (categoryEntity) {
-        queryBuilder = queryBuilder.andWhere(
-          'product.categoryId = :categoryId',
-          { categoryId: categoryEntity.id }
-        );
-      }
+    if (category && category.length > 0) {
+      queryBuilder = queryBuilder.andWhere('product.categoryId IN (:...category)', { category });
     }
 
     if (productName) {
       queryBuilder = queryBuilder.andWhere('product.name ILIKE :productName', {
-        productName: `%${productName}%`,
+        productName: `%${productName}%`
+      });
+    }
+
+    if (rating && rating.length > 0) {
+      queryBuilder = queryBuilder.andWhere('product.averageRating IN (:...rating)', { rating });
+    }
+
+    if (minPrice && maxPrice) {
+      queryBuilder = queryBuilder.andWhere('product.salesPrice BETWEEN :minPrice AND :maxPrice', {
+        minPrice,
+        maxPrice,
       });
     }
 
     if (sort) {
       const sortDirection = sort.toString().toUpperCase() as 'ASC' | 'DESC';
-      queryBuilder = queryBuilder.orderBy('product.salesPrice', sortDirection);
+      queryBuilder = queryBuilder.orderBy('product.createdAt', sortDirection);
     }
 
     const total = await queryBuilder.getCount();
-    const products = await queryBuilder.getMany();
+    const products = await paginate(queryBuilder, page, limit).getMany();
 
     return res.status(200).json({
       data: products,
-      total,
+      total
     });
   }
 );
--- src/middlewares/validateSearchParams.ts
+++ src/middlewares/validateSearchParams.ts
@@ -2,7 +2,11 @@ import { query, validationResult } from 'express-validator';
 import { Request, Response, NextFunction } from 'express';
 export const validateSearchParams = [
   query('keyword').optional().isString().withMessage('Keyword must be a string'),
-  query('category').optional().isString().withMessage('Category must be a string'),
+  query('category').optional().isArray().withMessage('Category must be an array'),
+  query('rating').optional().isArray().withMessage('Rating must be an array'),
+  query('page').optional().isString().withMessage('Page must be a number'),
+  query('minPrice').optional().isString().withMessage('minPrice must be a number'),
+  query('maxPrice').optional().isString().withMessage('maxPrice must be a number'),
   query('brand').optional().isString().withMessage('Brand must be a string'),
   query('productName').optional().isString().withMessage('Product name must be a string'),
   query('sort').optional().isIn(['asc', 'desc']).withMessage('Sort order must be either asc or desc'),
